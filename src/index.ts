6b3983edc1cf4d9db8a69063672dc3ca9d2f12d4
Maxime Malgorn <maxime.malgorn@laposte.net>
2021-02-19 19:20:04 +0100

    setup project

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Controls all interactions of the bot.
+ *
+ * @author Utarwyn
+ * @since 1.0.0
+ */
+class Monitoring {
+    constructor() {
+        console.log('Monitoring module OK!');
+    }
+}
+
+export = Monitoring;
